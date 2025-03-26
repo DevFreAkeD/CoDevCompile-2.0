a49aba8a724a6040f5dec70d1f0d7f35c667006d
Tushar Nagar <tusharrock146@gmail.com>
2025-03-27 00:24:02 +0530

    Added Language Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
@@ -8,11 +9,18 @@ export default function Home() {
       {/* Header */}
       <Header />
 
-      <main className="flex-1 flex items-center justify-center p-4">
-        <h1 className="text-xl font-bold text-zinc-800 dark:text-zinc-100">
-          Welcome to CoDevCompile!
-        </h1>
-      </main>
+      {/* Main Content with Sidebar*/}
+      <div className="flex flex-1 mt-16">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content */}
+        <main className="flex-1 flex items-center justify-center p-4">
+          <h1 className="text-xl font-bold text-zinc-800 dark:text-zinc-100">
+            Welcome to CoDevCompile!
+          </h1>
+        </main>
+      </div>
     </div>
   );
 }
