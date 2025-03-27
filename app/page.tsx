80e9f61159154cae9dcd613f83f3676f63589eea
Tushar Nagar <tusharrock146@gmail.com>
2025-03-28 00:01:10 +0530

    Added CodeEditor in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CodeEditor from "@/components/CodeEditor";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 
@@ -15,10 +16,8 @@ export default function Home() {
         <Sidebar />
 
         {/* Main Content */}
-        <main className="flex-1 flex items-center justify-center p-4">
-          <h1 className="text-xl font-bold text-zinc-800 dark:text-zinc-100">
-            Welcome to CoDevCompile!
-          </h1>
+        <main className="flex-1 flex items-center justify-center">
+          <CodeEditor />
         </main>
       </div>
     </div>
