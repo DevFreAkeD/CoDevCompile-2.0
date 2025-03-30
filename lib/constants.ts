bcb918fcd84c43365fb4d23217070dd1b7d1a7af
Tushar Nagar <tusharrock146@gmail.com>
2025-03-31 00:59:17 +0530

    Updated Snippets and added snippets for java and python

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,20 +1,35 @@
 export const HELLO_WORLD_CODES: Record<string, { title: string; code: string }> = {
     c: {
-      title: "main.c",
-      code: `#include <stdio.h>
-      
+        title: "Main.c",
+        code: `#include <stdio.h>  // Required for standard input and output functions (printf, scanf)
+
 int main() {
     printf("Hello, World!\\n");
     return 0;
 }`
     },
     cpp: {
-      title: "main.cpp",
-      code: `#include <iostream>
-  
+        title: "Main.cpp",
+        code: `#include <iostream>  // Required for input and output operations (std::cout, std::cin)
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }`
     },
-};  
+    python: {
+        title: "Main.py",
+        code: `# Python has built-in print function, so no imports are required
+print("Hello, World!")`,
+    },
+    java: { 
+        title: "Main.java",
+        code: `// The java.lang package is automatically imported, so System.out.println() works without an explicit import.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}`
+    },
+};
