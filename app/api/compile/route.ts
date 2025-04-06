d38e0e97531ef1fef2bfe23c1e5a58748f125ed5
Tushar Nagar <tusharrock146@gmail.com>
2025-04-07 00:38:29 +0530

    Added support for TypeScript and PHP languages

--- app/api/compile/route.ts
+++ app/api/compile/route.ts
@@ -14,6 +14,8 @@ export async function POST(req: Request) {
       java: { language: "java", version: "15.0.2" },
       csharp: { language: "csharp", version: "6.12.0" },
       javascript: { language: "javascript", version: "18.15.0" },
+      typescript: { language: "typescript", version: "5.0.3" },
+      php: { language: "php", version: "8.2.3" },
     };
 
     if (!languageMap[language]) {
